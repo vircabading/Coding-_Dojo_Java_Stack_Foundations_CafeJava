bee58c8a31b37fb3e2806959512f4285c7d6b706
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-26 17:46:54 -0800

    format variable section comments

--- CafeJava.java
+++ CafeJava.java
@@ -6,6 +6,26 @@
 public class CafeJava {
     // //// MAIN EXECUTABLE CODE ///////////////////////////////
     public static void main(String[] args) {
+
+        // **** Variable Declaration ***************************
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
+        
+        // **** Menu variables *********************************
+        double mochaPrice = 3.5;
+        
+        // **** Customer name variables ************************
+        String customer1 = "Cindhuri";
+        
+        // **** Order Completion variables *********************
+        boolean isReadyOrder1 = false;
+        
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        // ** Your customer interaction print statements will go here ** //
         
     }
 }
