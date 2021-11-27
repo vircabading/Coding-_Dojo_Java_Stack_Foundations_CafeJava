866b4adc7270d97c9fdb5f5360f64a3ea553097e
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-26 22:32:11 -0800

    Add Jimmy Orders Cafe Latte

--- CafeJava.java
+++ CafeJava.java
@@ -19,13 +19,13 @@ public class CafeJava {
         double mochaPrice = 3.5;                                // Create 3 more drink price variables
         double dripCoffeePrice = 1.75;                          //  for drip coffee, latte and cappuccino
         double cafeLattePrice = 3.75;
-        double capuccinoPrice = 4.0;
+        double cappuccinoPrice = 4.0;
         
         // **** Customer name variables ************************
         String customer1 = "Cindhuri";
-        String customer2 = "Sam";                               // Create 3 more customer variables
-        String customer3 = "Jimmy";                             //  for Sam, Jimmy and Noah
-        String customer4 = "Noah";
+        String customer2 = "Noah";                              // Create 3 more customer variables
+        String customer3 = "Sam";                               //  for Sam, Jimmy and Noah
+        String customer4 = "Jimmy";
         
         // **** Order Completion variables *********************
         boolean isReadyOrder1 = false;
@@ -53,9 +53,8 @@ public class CafeJava {
         } else {
             System.out.println(customer1 + pendingMessage);
         }
-        
         System.out.println("*****************************************************");
-        isReadyOrder1 = true;
+        isReadyOrder1 = true;                                   // Customer's order is now ready
         if (isReadyOrder1) {
             System.out.println(customer1 + readyMessage);
             System.out.println(displayTotalMessage + totalCostOrder1);
@@ -64,7 +63,77 @@ public class CafeJava {
         }
         System.out.println("");
 
-        // ** Your customer interaction print statements will go here ** //
+        System.out.println("/////////////////////////////////////////////////////");
+                                                                // Noah Orders a Cappuccino
+        System.out.println(generalGreeting + customer2);
+        System.out.println(customer2 + orderedMessage + "Cappuccino");
+        totalCostOrder2 += cappuccinoPrice;
+        System.out.println("Cappuccino" + costMessage + totalCostOrder2);
+        if (isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+            System.out.println(displayTotalMessage + totalCostOrder2);
+        } else {
+            System.out.println(customer2 + pendingMessage);
+        }
+        System.out.println("*****************************************************");
+        isReadyOrder2 = true;                                   // Customer's order is now ready
+        if (isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+            System.out.println(displayTotalMessage + totalCostOrder2);
+        } else {
+            System.out.println(customer2 + pendingMessage);
+        }
+        System.out.println("");
         
+        System.out.println("/////////////////////////////////////////////////////");
+                                                                // Sam Orders two Lattes
+        System.out.println(generalGreeting + customer3);
+        System.out.println(customer3 + orderedMessage + "Cafe Latte");
+        totalCostOrder3 += cafeLattePrice;
+        System.out.println("Cafe Latte" + costMessage + cafeLattePrice);
+        System.out.println(customer3 + orderedMessage + "Cafe Latte");
+        totalCostOrder3 += cafeLattePrice;
+        System.out.println("Cafe Latte" + costMessage + cafeLattePrice);
+        if (isReadyOrder3) {
+            System.out.println(customer3 + readyMessage);
+            System.out.println(displayTotalMessage + totalCostOrder3);
+        } else {
+            System.out.println(customer3 + pendingMessage);
+        }
+        System.out.println("*****************************************************");
+        isReadyOrder3 = true;                                   // Customer's order is now ready
+        if (isReadyOrder3) {
+            System.out.println(customer3 + readyMessage);
+            System.out.println(displayTotalMessage + totalCostOrder3);
+        } else {
+            System.out.println(customer3 + pendingMessage);
+        }
+        System.out.println("");
+
+        System.out.println("/////////////////////////////////////////////////////");
+                                                                // Jimmy just realized he was charged for a coffee but had ordered a latte
+        System.out.println(generalGreeting + customer4);
+        System.out.println(customer4 + orderedMessage + "Drip Coffee");
+        totalCostOrder4 += dripCoffeePrice;
+        System.out.println("Drip Coffee" + costMessage + dripCoffeePrice);
+        System.out.println(customer4 + orderedMessage + "Cafe Latte instead of a Drip Coffee");
+        totalCostOrder4 -= dripCoffeePrice;
+        totalCostOrder4 += cafeLattePrice;
+        System.out.println("Cafe Latte" + costMessage + cafeLattePrice);
+        if (isReadyOrder4) {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + totalCostOrder4);
+        } else {
+            System.out.println(customer4 + pendingMessage);
+        }
+        System.out.println("*****************************************************");
+        isReadyOrder4 = true;                                   // Customer's order is now ready
+        if (isReadyOrder4) {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + totalCostOrder4);
+        } else {
+            System.out.println(customer4 + pendingMessage);
+        }
+        System.out.println("");
     }
 }
